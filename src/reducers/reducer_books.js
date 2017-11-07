d1245edf2c8e74b514e38b16e94604cc83452dcb
Aurimas <aurimas@darguzis.com>
2017-11-07 15:42:58 +0000

    update

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'Javascript: The Good Parts'},
-    { title: 'Harry Potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Glass door'},
+    { title: 'Javascript: The Good Parts', pages: 101},
+    { title: 'Harry Potter', pages: 39},
+    { title: 'The Dark Tower', pages: 85},
+    { title: 'Glass door', pages: 1},
   ]
 }
